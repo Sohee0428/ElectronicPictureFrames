ae4352bcd15faaab5eb87339def5607b3e434071
Sohee <sohee3480@gmail.com>
2021-08-21 01:47:10 +0900

    버튼 설정하기

--- app/src/main/java/com/example/aop_part2_chapter05/MainActivity.kt
+++ app/src/main/java/com/example/aop_part2_chapter05/MainActivity.kt
@@ -1,9 +1,22 @@
 package com.example.aop_part2_chapter05
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val addPhotoBtn: Button by lazy {
+        findViewById<Button>(R.id.addPhotoBtn)
+    }
+
+    private val startPhotoFrameModeBtn: Button by lazy {
+        findViewById<Button>(R.id.startPhotoFromModeBtn)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
