ae4e645cfdf088fdab212e55585f94b9f270e2ff
Sohee <sohee3480@gmail.com>
2021-08-26 21:27:35 +0900

    사진이 넘어올 때 let문을 이용해서 null 유무에 따라 동작

--- app/src/main/java/com/example/aop_part2_chapter05/PhotoFrameActivity.kt
+++ app/src/main/java/com/example/aop_part2_chapter05/PhotoFrameActivity.kt
@@ -1,14 +1,23 @@
 package com.example.aop_part2_chapter05
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class PhotoFrameActivity : AppCompatActivity() {
+
+    private val photoList = mutableListOf<Uri>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_frame)
 
-
+        val size = intent.getIntExtra("photoListSize",0)
+        for (i in 0..size) {
+            intent.getStringExtra("photo$i")?.let {
+                photoList.add(Uri.parse(it))
+            }
+        }
 
     }
 }
