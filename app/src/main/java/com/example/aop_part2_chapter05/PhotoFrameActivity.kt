5487c78dc4f1c6d114227db32999f5400547ab93
Sohee <sohee3480@gmail.com>
2021-08-27 16:36:38 +0900

    timer를 켰다 껐다 하기 위한 설정

--- app/src/main/java/com/example/aop_part2_chapter05/PhotoFrameActivity.kt
+++ app/src/main/java/com/example/aop_part2_chapter05/PhotoFrameActivity.kt
@@ -3,7 +3,9 @@ package com.example.aop_part2_chapter05
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
+import java.util.*
 import kotlin.concurrent.timer
 
 class PhotoFrameActivity : AppCompatActivity() {
@@ -20,13 +22,12 @@ class PhotoFrameActivity : AppCompatActivity() {
 
     private var currentPosition = 0
 
+    private var timer: Timer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_frame)
-
         getPhotoUriPromIntent()
-        startTimer()
-
     }
 
 
@@ -40,9 +41,8 @@ class PhotoFrameActivity : AppCompatActivity() {
     }
 
     private fun startTimer(){
-        timer(period = 5000) {
+        timer= timer(period = 5000) {
             runOnUiThread {
-
                 val current = currentPosition
                 val next = if(photoList.size <= currentPosition +1) 0 else currentPosition + 1
 
