3ccea576c1a48406fd978f8ef9d4b6a980b6e30c
Sohee <sohee3480@gmail.com>
2021-08-27 16:14:44 +0900

    ImageView 변수로 설정

--- app/src/main/java/com/example/aop_part2_chapter05/PhotoFrameActivity.kt
+++ app/src/main/java/com/example/aop_part2_chapter05/PhotoFrameActivity.kt
@@ -3,10 +3,22 @@ package com.example.aop_part2_chapter05
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import kotlin.concurrent.timer
 
 class PhotoFrameActivity : AppCompatActivity() {
 
     private val photoList = mutableListOf<Uri>()
+
+    private val backgroundPhotoImageView: ImageView by lazy {
+        findViewById<ImageView>(R.id.backgroundPhotoImageView)
+    }
+
+    private val photoImageView: ImageView by lazy {
+        findViewById<ImageView>(R.id.photoImageView)
+    }
+
+    private var currentPosition = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
